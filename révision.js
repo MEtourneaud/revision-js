a61a8971c297b318303c759ac2b1161e04ff9b7a
Mathieu Etourneaud <m.etourneaud@gmail.com>
2024-10-03 15:33:02 +0200

    Modification variables

---
+++ révision.js
@@ -0,0 +1,15 @@
+//Bibliothèque de 500 livres
+//Acheter 50 livres de plus
+//En jeter 10
+//Racheter 5 des livres jetés
+
+let totalLivres = 500
+
+totalLivres += 50
+totalLivres -= 10
+totalLivres += 5
+
+console.log(totalLivres)
+
+let affichageTotalLivres = "Notre bibliothèque possède " + totalLivres + " livres"
+console.log(affichageTotalLivres)
