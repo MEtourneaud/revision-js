40ab645a2d9fec81c4f2fe4e051d92a23f54630c
Mathieu Etourneaud <m.etourneaud@gmail.com>
2024-10-03 15:45:32 +0200

    Objets JavaScript

--- révision.js
+++ révision.js
@@ -1,3 +1,5 @@
+//===>Modification variables
+
 //Bibliothèque de 500 livres
 //Acheter 50 livres de plus
 //En jeter 10
@@ -9,7 +11,23 @@ totalLivres += 50
 totalLivres -= 10
 totalLivres += 5
 
-console.log(totalLivres)
+// console.log(totalLivres)
 
 let affichageTotalLivres = "Notre bibliothèque possède " + totalLivres + " livres"
-console.log(affichageTotalLivres)
+// console.log(affichageTotalLivres)
+
+//==>Objets JavaScript
+
+let ticket = {
+  nomFilm: "Alien",
+  prix: 12,
+  numeroSalle: 2,
+}
+
+let nom = "Mathieu"
+
+// Bonjour nom, votre film NOMDEFILM est en salle NUMERODELASALE
+
+let texteAffichage =
+  "Bonjour " + nom + ", votre film " + ticket.nomFilm + " est en salle " + ticket.numeroSalle + "."
+console.log(texteAffichage)
